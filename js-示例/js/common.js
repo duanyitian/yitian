1406dc0e58c7dbf2a73c3140a0c87c6d7b9780ae
duan <1359720348@qq.com>
2019-01-25 15:59:51 +0800

    暂存

---
+++ js-示例/js/common.js
@@ -0,0 +1,13 @@
+// i = 0;
+//   var rotate = 0;
+//
+//   function Redenvelope5() {
+//       $('.Redenvelope5 img:nth-child(3)').css("transform", "rotate(" + rotate + "deg)");
+//       setTimeout(function() {
+//           i++;
+//           rotate = rotate + 1;
+//           Redenvelope5();
+//       }, 45)
+//
+//
+//   }
